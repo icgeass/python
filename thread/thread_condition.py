abcb3b6abf240afb04026378c4cd8a3a9c450881
you <you@example.com>
2019-04-18 11:38:12 +0800

    threading.Condition-remark

--- thread/thread_condition.py
+++ thread/thread_condition.py
@@ -1,6 +1,29 @@
 #! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from threading import Condition
 import threading
+
+# https://www.anshu.com/p/5d257
+# https://www.cnblogs.com/yoyoketang/p/8337118.html
+
+
+'''
+acquire([timeout])/release(): 调用关联的锁的相应方法。 
+wait([timeout]): 调用这个方法将使线程进入Condition的等待池等待通知，并释放锁。
+    使用前线程必须已获得锁定，否则将抛出异常。 
+notify(): 调用这个方法将从等待池挑选一个线程并通知，收到通知的线程将自动调用
+    acquire()尝试获得锁定（进入锁定池）；其他线程仍然在等待池中。调用这个方法不会
+    释放锁定。使用前线程必须已获得锁定，否则将抛出异常。 
+notifyAll(): 调用这个方法将通知等待池中所有的线程，这些线程都将进入锁定池
+    尝试获得锁定。调用这个方法不会释放锁定。使用前线程必须已获得锁定，否则将抛出异常。
+
+作者：StormZhu
+链接：https://www.jianshu.com/p/5d2579938517
+来源：简书
+简书著作权归作者所有，任何形式的转载都请联系作者获得授权并注明出处。
+'''
+
 
 class CountDownLatch:
 
@@ -10,10 +33,10 @@ class CountDownLatch:
 
     def await(self):
         try:
-            self.condition.acquire()
+            self.condition.acquire()  # 获得的是同一个共享锁
             while self.count > 0:
                 print '----await: wait begin'
-                self.condition.wait()
+                self.condition.wait()  # 拿到锁也不要，还回去给其他线程，自己进入等待池等待通知
                 print '----await: wait end'
         finally:
             self.condition.release()
@@ -57,10 +80,6 @@ if __name__ == '__main__':
     threading.Thread(target=test, args=(4, countDown,)).start()
     threading.Thread(target=test, args=(5, countDown,)).start()
 
-
     import time
+
     time.sleep(10)
-
-
-
-
